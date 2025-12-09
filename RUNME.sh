5831c76cffa8db4dd6d009445c9c063d2ec57129
Pim Snel <post@pimsnel.com>
2025-12-09 17:14:09 +0100

    chore: add RUNME.sh script

---
+++ RUNME.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+#(C)2019-2022 Pim Snel - https://github.com/mipmip/RUNME.sh
+CMDS=();DESC=();NARGS=$#;ARG1=$1;make_command(){ CMDS+=($1);DESC+=("$2");};usage(){ printf "\nUsage: %s [command]\n\nCommands:\n" $0;line="              ";for((i=0;i<=$(( ${#CMDS[*]} -1));i++));do printf "  %s %s ${DESC[$i]}\n" ${CMDS[$i]} "${line:${#CMDS[$i]}}";done;echo;};runme(){ if test $NARGS -eq 1;then eval "$ARG1"||usage;else usage;fi;}
+
+##### PLACE YOUR COMMANDS BELOW #####
+
+
+make_command "buildrun" "Build and run the module set in \$MODULE"
+buildrun(){
+
+  nix flake update && nom build .#nixosConfigurations.${MODULE}.config.system.build.vm
+  ./result/bin/run-nixos-vm
+}
+
+##### PLACE YOUR COMMANDS ABOVE #####
+
+runme
